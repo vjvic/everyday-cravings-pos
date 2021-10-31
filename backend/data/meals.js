3fcba27555aed919e41b4450a7a554763b9acff6
vjvic <victorsulit0@gmail.com>
2021-10-31 20:45:18 +0800

    Add sample data

---
+++ backend/data/meals.js
@@ -0,0 +1,44 @@
+const meals = [
+  {
+    name: "Burger",
+    image:
+      "https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8YnVyZ2VyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    description:
+      "A hamburger (or burger for short) is a food, typically considered a sandwich, consisting of one or more cooked patties—usually ground meat",
+    category: "lunch",
+    price: 500,
+    countInStock: 3,
+  },
+  {
+    name: "Chicken",
+    image:
+      "https://insanelygoodrecipes.com/wp-content/uploads/2020/04/Fried_Chicken-1024x536.png",
+    description:
+      "fried chicken, is a dish consisting of chicken pieces that have been coated with seasoned flour or batter and pan-fried, deep fried, pressure fried, or air fried.",
+    category: "dinner",
+    price: 500,
+    countInStock: 3,
+  },
+  {
+    name: "Steak",
+    image:
+      "https://www.seriouseats.com/thmb/uGGwEqPZf7PzhES1ZCAqHLgCbG8=/1500x844/smart/filters:no_upscale()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__recipes__images__2015__05__Anova-Steak-Guide-Sous-Vide-Photos15-beauty-159b7038c56a4e7685b57f478ca3e4c8.jpg",
+    description:
+      "a slice of meat cut from a fleshy part of a beef carcass. b : a similar slice of a specified meat other than beef ham steak",
+    category: "dinner",
+    price: 500,
+    countInStock: 3,
+  },
+  {
+    name: "Pancakes",
+    image:
+      "https://images-gmi-pmc.edge-generalmills.com/edfaaf9f-9bde-426a-8d67-3284e9e496ae.jpg",
+    description:
+      "A pancake is a breakfast dish, a flat cake that's made by pouring batter into a hot pan and frying it on both sides",
+    category: "breakfast",
+    price: 500,
+    countInStock: 3,
+  },
+];
+
+export default meals;
