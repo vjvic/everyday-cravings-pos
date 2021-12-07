31bce462754edca3413760358d58d31e39f1fb99
vjvic <victorsulit0@gmail.com>
2021-12-07 21:29:07 +0800

    Changing folder structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,23 +3,25 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 /* import { deepOrange } from "@mui/material/colors"; */
 import Layout from "./components/Layout/Layout";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import BookPage from "./Pages/User/BookPage";
-import HomePage from "./Pages/User/HomePage";
-import CartPage from "./Pages/User/CartPage";
-import FavoritesPage from "./Pages/User/FavoritesPage";
-import ResultsPage from "./Pages/User/ResultsPage";
-import AboutPage from "./Pages/User/AboutPage";
-import Dashboard from "./Pages/Admin/DashboardPage";
-import MealsPage from "./Pages/Admin/MealsPage";
-import Orders from "./Pages/Admin/OrdersPage";
-import MealDetailsPage from "./Pages/User/MealDetailsPage";
-import LoginPage from "./Pages/User/LoginPage";
-import RegisterPage from "./Pages/User/RegisterPage";
+import {
+  AboutPage,
+  BookPage,
+  CartPage,
+  DashboardPage,
+  FavoritesPage,
+  HomePage,
+  LoginPage,
+  MealDetailsPage,
+  MealsPage,
+  OrdersPage,
+  ProfilePage,
+  RegisterPage,
+  UserListPage,
+  ResultsPage,
+} from "./pages";
+import { CssBaseline } from "@mui/material";
 import PrivateRoute from "./components/Route/PrivateRoute";
 import AdminRoute from "./components/Route/AdminRoute";
-import UsersListPage from "./Pages/Admin/UsersListPage";
-import CssBaseline from "@mui/material/CssBaseline";
-import ProfilePage from "./Pages/User/ProfilePage";
 
 const secondary = "#DE8538";
 
@@ -60,13 +62,17 @@ function App() {
             <PrivateRoute exact path="/book" component={BookPage} />
             <PrivateRoute exact path="/profile" component={ProfilePage} />
             {/* Admin Route */}
-            <AdminRoute exact path="/admin/dashboard" component={Dashboard} />
+            <AdminRoute
+              exact
+              path="/admin/dashboard"
+              component={DashboardPage}
+            />
             <AdminRoute exact path="/admin/meals" component={MealsPage} />
-            <AdminRoute exact path="/admin/orders" component={Orders} />
+            <AdminRoute exact path="/admin/orders" component={OrdersPage} />
             <AdminRoute
               exact
               path="/admin/user-list"
-              component={UsersListPage}
+              component={UserListPage}
             />
             {/* User Auth Route*/}
             <Route path="/login" component={LoginPage} />
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,31 @@
+import AboutPage from "./AboutPage/AboutPage";
+import BookPage from "./BookPage/BookPage";
+import CartPage from "./CartPage/CartPage";
+import DashboardPage from "./DashboardPage/DashboardPage";
+import FavoritesPage from "./FavoritesPage/FavoritesPage";
+import HomePage from "./HomePage/HomePage";
+import LoginPage from "./LoginPage/LoginPage";
+import MealDetailsPage from "./MealDetailsPage/MealDetailsPage";
+import MealsPage from "./MealsPage/MealsPage";
+import OrdersPage from "./OrdersPage/OrdersPage";
+import ProfilePage from "./ProfilePage/ProfilePage";
+import RegisterPage from "./RegisterPage/RegisterPage";
+import UserListPage from "./UserListPage/UsersListPage";
+import ResultsPage from "./ResultsPage/ResultsPage";
+
+export {
+  AboutPage,
+  BookPage,
+  CartPage,
+  DashboardPage,
+  FavoritesPage,
+  HomePage,
+  LoginPage,
+  MealDetailsPage,
+  MealsPage,
+  OrdersPage,
+  ProfilePage,
+  RegisterPage,
+  UserListPage,
+  ResultsPage,
+};
